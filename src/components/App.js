09ab395d7ce878419beef99e26ac18d0cc70484c
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-19 18:26:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,25 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Personalized Greeting App</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleInputChange}
+      />
+      {name && <h2>Hello, {name}!</h2>}
     </div>
-  )
-}
+  );
+};
 
 export default App
