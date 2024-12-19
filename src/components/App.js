16b91661d7836d5f56600f60d4b3cda3c029a47b
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-19 19:33:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ const App = () => {
       />
       {/* Always render a message to avoid empty states */}
       <h2>
-        {name.trim() ? `Hello n!` : "Please enter your name."}
+        {name.trim() ? `Hello name!` : "Please enter your name."}
   
       </h2>
     </div>
